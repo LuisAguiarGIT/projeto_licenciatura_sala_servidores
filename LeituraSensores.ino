40c4afa4b4964479651dcdb7c0fe5c65b4312bab
Luis Aguiar <luisaguiar96@hotmail.com>
2021-07-16 19:42:03 +0100

    Fixing memory leaks; still may be more

--- LeituraSensores.ino
+++ LeituraSensores.ino
@@ -1,99 +1,121 @@
-boolean configuraSensores() {
-  File ficheiroConfig = LittleFS.open("/config.json", "r");
-  if (!ficheiroConfig) {
-    Serial.println("Nao existe ficheiro config");
-    return false;
+void enviaDados(String tipoDeSensor, String itemZabbix, boolean medeTemperatura, boolean medeHumidade, uint8_t pinSensor) {
+  String stringCopy  = itemZabbix;
+  String appendTemp = "temp";
+  String appendHum  = "humi";
+
+  if (tipoDeSensor = "DHT11") {
+    if (medeTemperatura && medeHumidade) {
+      DHT.read11(pinSensor1);
+      float temperatura = DHT.temperature;
+      float humidade = DHT.humidity;
+
+      String itemZabbixTemp = stringCopy + appendTemp;
+      enviaDadosSensor(itemZabbixTemp, temperatura);
+
+      String itemZabbixHumi = stringCopy + appendHum;
+      enviaDadosSensor(itemZabbixHumi, humidade);
+    }
   }
 
-  size_t size = ficheiroConfig.size();
-  if (size > 4096) {
-    Serial.println("Ficheiro de configuração demasiado grande");
-    return false;
-  }
-
-  std::unique_ptr<char[]> buf(new char[size]);
-  ficheiroConfig.readBytes(buf.get(), size);
-  //Serial.println(buf.get());
-
-  StaticJsonDocument<4096> doc;
-
-  auto error = deserializeJson(doc, buf.get());
-  if (error) {
-    Serial.println("Erro na analise do ficheiro json");
-    return false;
-  }
-
-  tipoSensor = (const char*)doc["sensor1"]["tipoSensor"];
-  itemZabbix = (const char*)doc["sensor1"]["itemZabbix"];
-  pinSensor  = (uint8_t)doc["sensor1"]["pinSensor"];
-  medeTemperatura = (boolean)doc["sensor1"]["medeTemperatura"];
-  medeHumidade = (boolean)doc["sensor1"]["medeHumidade"];
-  intervaloMedicao = (long)doc["sensor1"]["intervaloMedicao"];
-
-//  Serial.println(tipoSensor);
-//  Serial.println(itemZabbix);
-//  Serial.println(pinSensor);
-//  Serial.println(medeTemperatura);
-//  Serial.println(medeHumidade);
-//  Serial.println(intervaloMedicao);
-
-  ficheiroConfig.close();
-  return true;
 }
 
-void leValorSensor(const char* tipoDeSensor, boolean medeTemperatura, boolean medeHumidade, uint8_t pinSensor) {
-  if (tipoDeSensor = "DHT11") {
-    float temperatura = 0;
-    float humidade    = 0;
-    DHT dht(pinSensor, DHTTYPE11);
-    dht.begin();
+//void verificaIntervalo(long currentMillis, long intervaloMedicao, const char* tipoDeSensor, boolean medeTemperatura, boolean medeHumidade, uint8_t pinSensor, const char* itemZabbix) {
+//  if (currentMillis - previousMillis > intervaloMedicao) {
+//    previousMillis = currentMillis;
+//    if (isConnected) {
+//      enviaDados(tipoDeSensor, medeTemperatura, medeHumidade, pinSensor, itemZabbix);
+//    } else {
+//      setup();
+//    }
+//  }
+//}
 
-    if (medeTemperatura && medeHumidade) {
-      temperatura = dht.readTemperature();
-      humidade    = dht.readHumidity();
-
-      Serial.println(temperatura);
-      //Serial.println(humidade);
-      enviaDadosSensor(itemZabbix, temperatura);
-    } else if (medeTemperatura) {
-      temperatura = dht.readTemperature();
-
-      Serial.println(temperatura);
-    } else if (medeHumidade) {
-      humidade = dht.readHumidity();
-
-      Serial.println(humidade);
-    } else {
-      Serial.println("Erro na configuração");
-    }
-  }
-
-  if (tipoDeSensor = "DHT22") {
-    float temperatura = 0;
-    float humidade    = 0;
-    DHT dht(pinSensor, DHTTYPE22);
-    dht.begin();
-
-    if (medeTemperatura && medeHumidade) {
-      temperatura = dht.readTemperature();
-      humidade    = dht.readHumidity();
-
-      Serial.println(temperatura);
-      Serial.println(humidade);
-    } else if (medeTemperatura) {
-      temperatura = dht.readTemperature();
-
-      Serial.println(temperatura);
-    } else if (medeHumidade) {
-      humidade = dht.readHumidity();
-
-      Serial.println(humidade);
-    } else {
-      Serial.println("Erro na configuração");
-    }
-  }
+//void enviaDados(const char* tipoDeSensor, boolean medeTemperatura, boolean medeHumidade, uint8_t pinSensor, const char* itemZabbix) {
+//  float temperatura       = 0;
+//  float humidade          = 0;
+//  const char* appendTemp  = "temp";
+//  const char* appendHum   = "humi";
+//  //const char* itemZabbixCopy = itemZabbix;
+//  char buf[30] = {0};
+//  
+//  if (tipoDeSensor = "DHT11") {
+//    DHT dht(pinSensor, DHTTYPE11);
+//    dht.begin();
+//
+//    if (medeTemperatura && medeHumidade) {
+//      temperatura = dht.readTemperature();
+//      humidade    = dht.readHumidity();
+//
+////      Serial.println(itemZabbixCopy);
+////      strcpy(buf, itemZabbixCopy);
+////      strcat(buf, appendTemp);
+//      
+//      enviaDadosSensor(itemZabbix, temperatura);
+//
+////      strcpy(buf, itemZabbixCopy);
+////      strcat(buf, appendHum);
+////      
+////      enviaDadosSensor(buf, humidade);
+//    } else if (medeTemperatura && !medeHumidade) {
+//      temperatura = dht.readTemperature();
+//
+//      //strcpy(buf, itemZabbixCopy);
+//      //strcat(buf, appendTemp);
+//      
+//      enviaDadosSensor(buf, temperatura);
+//    } else if (medeHumidade && !medeTemperatura) {
+//      humidade = dht.readHumidity();
+//      
+//      //strcpy(buf, itemZabbixCopy);
+//      //strcat(buf, appendHum);
+//      
+//      enviaDadosSensor(buf, humidade);
+//    } else {
+//      Serial.print("Erro na configuração do sensor DHT11 no pino:");
+//      Serial.println(pinSensor);
+//    }
+//  }
+//
+//  else if (tipoDeSensor = "DHT22") {
+//    DHT dht(pinSensor, DHTTYPE22);
+//    dht.begin();
+//
+//    if (medeTemperatura && medeHumidade) {
+//      temperatura = dht.readTemperature();
+//      humidade    = dht.readHumidity();
+//      
+//      //strcpy(buf, itemZabbixCopy);
+//      //strcat(buf, appendTemp);
+//      
+//      enviaDadosSensor(buf, temperatura);
+//   
+//      //strcpy(buf, itemZabbixCopy);
+//      //strcat(buf, appendHum);
+//      
+//      enviaDadosSensor(buf, humidade);
+//    } else if (medeTemperatura && !medeHumidade) {
+//      temperatura = dht.readTemperature();
+//
+//      //strcpy(buf, itemZabbixCopy);
+//      //strcat(buf, appendTemp);
+//      
+//      //enviaDadosSensor(buf, temperatura);
+//    } else if (medeHumidade && !medeTemperatura) {
+//      humidade = dht.readHumidity();
+//    
+//     // strcpy(buf, itemZabbixCopy);
+//     // strcat(buf, appendHum);
+//      
+//     // enviaDadosSensor(buf, humidade);
+//    } else {
+//      Serial.print("Erro na configuração do sensor DHT22 no pino:");
+//      Serial.println(pinSensor);
+//    }
+//  } else {
+//    Serial.println("Sensor introduzido não é possível avaliar");
+//  }
 
 //  if (tipoDeSensor == "Reed") {
 //    // TODO
 //  }
-}
+//}
